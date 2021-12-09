a6e3b02c11689c313ad61fb7ce424d4ac77c7e9e
Nastia <anastasiiamarets@nltu.lviv.ua>
2021-12-09 13:08:05 +0200

    Додала змінну рік народження

--- Zalik/Program.cs
+++ Zalik/Program.cs
@@ -12,6 +12,8 @@ namespace Zalik
         {
             Console.Write("Введіть ім'я: ");
             string name = Console.ReadLine();
+            Console.Write("Введіть рік народження: ");
+            int year = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Введіть вік: ");
             int age = Convert.ToInt32(Console.ReadLine());
